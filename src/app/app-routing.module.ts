dc530d80265cbe30f818a31d761a99ed74cc1465
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-03-15 15:41:40 -0300

    Rotas finalizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TelaPrincipalComponent } from './tela-principal/tela-principal.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
+import { HistoricoPrecosComponent } from './historico-precos/historico-precos.component';
 
 const routes: Routes = [
   { path: '', component: TelaPrincipalComponent },
-  { path: 'lista-usuarios', component: ListaUsuariosComponent }
+  { path: 'lista-usuarios', component: ListaUsuariosComponent },
+  { path: 'historico-preco', component: HistoricoPrecosComponent}
 ];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
